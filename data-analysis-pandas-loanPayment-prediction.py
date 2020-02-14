a47ac7b6f8b7429745a6559643b4fd0fc377be63
Brian Pan <brianpanzifan@gmail.com>
2020-02-14 02:53:06 -0500

    Continue the project

--- data-analysis-pandas-loanPayment-prediction.py
+++ data-analysis-pandas-loanPayment-prediction.py
@@ -31,7 +31,7 @@ from sklearn.metrics import f1_score, recall_score, precision_score, roc_auc_sco
 from sklearn.metrics import classification_report, precision_recall_curve, roc_curve, auc
 
 
-# In[2]:
+# In[ ]:
 
 
 # Next step:
@@ -42,7 +42,7 @@ df = pd.read_csv('data.csv',low_memory=False)
 # In[ ]:
 
 
-# Have a look at data frame
+# Have a look at data structure
 df.head()
 
 
@@ -56,6 +56,72 @@ df.shape
 # In[ ]:
 
 
-# Check the columns' info
+# Check the column info in list form
 list(df.columns)
+
+
+# In[ ]:
+
+
+# Check the target variable
+df['loan_status'].value_counts()
+
+
+# ##### From the values under loan_status col we can see there are some people who did not meet the credit policy, these data should be removed.
+
+# In[ ]:
+
+
+# Remove the improper data set
+df = df.loc[df['loan_status'].isin(['Fully Paid', 'Charged Off'])]
+
+
+# In[ ]:
+
+
+# Check the new one
+df['loan_status'].value_counts()
+
+
+# In[ ]:
+
+
+# After rearranged the target variable,
+# check the missing values, and return the missing rate.
+# Define a function:
+def missingVals(df):
+    '''
+    This function return a table of number of missing values, 
+    and it's percentage.
+    '''
+    # Proportion of missing values
+    propMissingVals = df.isnull.sum() / len(df) * 100
+    
+    # Create a table
+    tableMissingVals = pd.concat([df.isnull().sum(), propMissingVals], axis=1)
+    
+    # Rename the cols
+    renTableMissVals = tableMissingVals.rename(
+        columns = {0: "Missing Value Total", 1: "Percentage (%) of Missing Value"})
+    
+    # Sort the table by percentage of missing values
+    # in descending order (ie. highest ot lowest).
+    renTableMissVals = renTableMissVals[
+        renTableMissVals.iloc[:,1] != 0].sort_values(
+        "Percentage (%) of Missing Value",
+        ascending = False).round(2)
+    
+    # Print conclustion messages for readers.
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"      
+            "There are " + str(renTableMissVals.shape[0]) +
+              " columns that have missing values.")
+    
+    # Return the data frame in table with missing values not removed.
+    return renTableMissVals
+
+
+# In[ ]:
+
+
+
 
