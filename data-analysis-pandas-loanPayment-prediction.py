ee20e29825a9066fec706a186d68ab3f81bd298f
Brian Pan <brianpanzifan@gmail.com>
2020-02-15 01:21:30 -0500

    Con'd coding

--- data-analysis-pandas-loanPayment-prediction.py
+++ data-analysis-pandas-loanPayment-prediction.py
@@ -33,7 +33,7 @@ from sklearn.metrics import classification_report, precision_recall_curve, roc_c
 
 # ### First stage
 
-# In[ ]:
+# In[2]:
 
 
 # Next step:
@@ -41,28 +41,28 @@ from sklearn.metrics import classification_report, precision_recall_curve, roc_c
 df = pd.read_csv('data.csv',low_memory=False)
 
 
-# In[ ]:
+# In[3]:
 
 
 # Have a look at data structure
 df.head()
 
 
-# In[ ]:
+# In[4]:
 
 
 # Check the number of rows and cols
 df.shape
 
 
-# In[ ]:
+# In[5]:
 
 
 # Check the column info in list form
 list(df.columns)
 
 
-# In[ ]:
+# In[6]:
 
 
 # Check the target variable
@@ -71,21 +71,21 @@ df['loan_status'].value_counts()
 
 # ##### From the values under loan_status col we can see there are some people who did not meet the credit policy, these data should be removed.
 
-# In[ ]:
+# In[7]:
 
 
 # Remove the improper data set
 df = df.loc[df['loan_status'].isin(['Fully Paid', 'Charged Off'])]
 
 
-# In[ ]:
+# In[8]:
 
 
 # Check the new one
 df['loan_status'].value_counts()
 
 
-# In[ ]:
+# In[9]:
 
 
 # After rearranged the target variable,
@@ -96,8 +96,11 @@ def missingVals(df):
     This function return a table of number of missing values, 
     and it's percentage.
     '''
+    # Total missing values
+    missingVals = df.isnull().sum()
+        
     # Proportion of missing values
-    propMissingVals = df.isnull.sum() / len(df) * 100
+    propMissingVals = missingVals / len(df) * 100
     
     # Create a table
     tableMissingVals = pd.concat([df.isnull().sum(), propMissingVals], axis=1)
@@ -122,7 +125,14 @@ def missingVals(df):
     return renTableMissVals
 
 
-# In[ ]:
+# In[10]:
+
+
+missing = missingVals(df)
+print(missing)
+
+
+# In[11]:
 
 
 # Drop the columns with high percentage (80%)
@@ -132,14 +142,14 @@ missingCols = list(missing.index[
 dfSub = df.drop(columns = missingCols)
 
 
-# In[ ]:
+# In[12]:
 
 
 # Check
 missingVals(dfSub)
 
 
-# In[ ]:
+# In[13]:
 
 
 # After check missings, check duplicated values and drop.
@@ -147,9 +157,50 @@ dfSub[dfSub.duplicated()]
 dfSub.drop_duplicates(inplace= True)
 
 
-# In[ ]:
+# In[14]:
 
 
 # Check once, how many rows and cols remaining.
 dfSub.shape
+
+
+# ### Stage Two: Exploratory Data Analysis
+
+# In[15]:
+
+
+# Check the data is balanced or imbalanced
+# Create a new col named charged_off, where fully paid is 0 and charged off is 1.
+dfSub["charged_off"] = dfSub["loan_status"].apply(lambda x: 1 if x == "Charged Off" else 0)
+
+
+# In[16]:
+
+
+# charge off rate (in %)
+chargeOffRate = dfSub["charged_off"].sum() / dfSub.shape[0] * 100
+chargeOffRate.round(2)
+
+
+# The charge off rate is relatively high (14%), we consider the data set as imbalanced.
+
+# In[17]:
+
+
+# Drop the cols that potentially cause data leakage.
+leakCols = ["total_pymnt_inv","total_rec_prncp","funded_amnt","funded_amnt_inv","total_pymnt",
+            "total_rec_int","total_rec_late_fee","recoveries","collection_recovery_fee",
+            "last_pymnt_amnt","chargeoff_within_12_mths","debt_settlement_flag"]
+
+
+# In[18]:
+
+
+dfSub.drop(columns = leakCols, inplace=True)
+
+
+# In[24]:
+
+
+#dfSub.shape
 
